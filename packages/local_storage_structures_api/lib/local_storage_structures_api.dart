0bbd9e27a3656a97b49e2152dcb0d02c73f555b0
Michal Krol <hello@krolmic.dev>
2024-11-08 22:06:17 +0700

    chore 🔧: Add local storage structures api package

---
+++ packages/local_storage_structures_api/lib/local_storage_structures_api.dart
@@ -0,0 +1,4 @@
+/// A Flutter implementation of the StructuresApi that uses local storage.
+library;
+
+export 'src/local_storage_structures_api.dart';
---
+++ packages/local_storage_structures_api/lib/src/local_storage_structures_api.dart
@@ -0,0 +1,7 @@
+/// {@template local_storage_structures_api}
+/// A Flutter implementation of the StructuresApi that uses local storage.
+/// {@endtemplate}
+class LocalStorageStructuresApi {
+  /// {@macro local_storage_structures_api}
+  const LocalStorageStructuresApi();
+}
