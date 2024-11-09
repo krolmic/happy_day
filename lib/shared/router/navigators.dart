e5048225a1ca30427287343b375be1ebc9d5e22f
Michal Krol <hello@krolmic.dev>
2024-11-09 15:14:44 +0700

    chore: fix linting, remove navigators

--- lib/shared/router/navigators.dart
+++
@@ -1,5 +0,0 @@
-part of 'router.dart';
-
-final _rootNavigatorKey = GlobalKey<NavigatorState>();
-final _shellNavigatorDailyStructuresKey =
-    GlobalKey<NavigatorState>(debugLabel: 'shellDailyStructures');
