01a5e4d6a815ba7a8754a4590b0b88cb994f8944
Michal Krol <hello@krolmic.dev>
2024-11-30 18:36:00 +0700

    feat(onboarding): add onboarding repository package

---
+++ packages/onboarding_repository/lib/src/onboarding_repository.dart
@@ -0,0 +1,7 @@
+/// {@template onboarding_repository}
+/// Repository for managing onboarding data
+/// {@endtemplate}
+class OnboardingRepository {
+  /// {@macro onboarding_repository}
+  const OnboardingRepository();
+}
---
+++ packages/onboarding_repository/test/src/onboarding_repository_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:onboarding_repository/onboarding_repository.dart';
+
+void main() {
+  group('OnboardingRepository', () {
+    test('can be instantiated', () {
+      expect(OnboardingRepository(), isNotNull);
+    });
+  });
+}
