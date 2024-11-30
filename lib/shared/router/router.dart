bdeb910c1ece55d96f8cbbde0c015c473c020dac
Michal Krol <hello@krolmic.dev>
2024-11-30 18:37:21 +0700

    feat(onboarding): add onboading route

--- lib/shared/router/router.dart
+++ lib/shared/router/router.dart
@@ -2,17 +2,20 @@ import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
 import 'package:happy_day/daily_structures/view/daily_structures_page.dart';
 import 'package:happy_day/edit_structure/view/edit_structure_page.dart';
+import 'package:happy_day/onboarding/view/onboarding_page.dart';
 import 'package:happy_day/shared/router/routes_names.dart';
 import 'package:happy_day/structure_details/view/structure_details_page.dart';
 import 'package:structures_api/structures_api.dart';
 
 part 'routes/daily_structures.dart';
 part 'routes/edit_structure.dart';
+part 'routes/onboarding.dart';
 part 'routes/structure_details.dart';
 
 final goRouter = GoRouter(
   initialLocation: '/${RoutesNames.dailyStructures}',
   routes: [
+    _onboardingRoute,
     _dailyStructuresRoute,
     _structureDetailsRoute,
     _editStructureRoute,
--- lib/shared/router/routes_names.dart
+++ lib/shared/router/routes_names.dart
@@ -1,6 +1,7 @@
 class RoutesNames {
   RoutesNames._();
 
+  static const String onboarding = 'onboarding';
   static const String dailyStructures = 'daily-structures';
   static const String editStructure = 'edit-structure';
   static const String structureDetails = 'structure-details';
